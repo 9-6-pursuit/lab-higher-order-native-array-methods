5e0b047803caaa5b2ff7acd924f61ed8e141284b
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-02-19 18:03:50 -0500

    All tests pass

--- src/06-sort.js
+++ src/06-sort.js
@@ -13,7 +13,9 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  return songs.sort((a,b) => a.runtimeInSeconds - b.runtimeInSeconds)
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,7 +25,38 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  return songs.sort((a,b) => {
+  return (b.artist.toUpperCase().localeCompare(a.artist.toUpperCase()))
+  })
+
+                              // OR
+
+
+  // return songs.sort((a,b) =>{
+  //   if(b.artist.toUpperCase() < a.artist.toUpperCase()){
+  //     return -1
+  //   }
+  //   else if(b.artist.toUpperCase() > a.artist.toUpperCase()){
+  //     return 1
+  //   }
+  //   else {
+  //     return 0
+  //   }
+
+  // })
+
+                              //  OR
+
+//   return songs.sort((a,b) => {
+  
+//     const nameA = a.artist.toUpperCase(); // ignore upper and lowercase
+//     const nameB = b.artist.toUpperCase(); // ignore upper and lowercase
+//     if (nameA > nameB) {
+//       return -1;
+//     }
+// })
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +66,11 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  return songs.sort((a,b) => {
+  return (a.title.toUpperCase().localeCompare(b.title.toUpperCase()))
+})
+}
 
 module.exports = {
   sortByRuntimeAscending,
