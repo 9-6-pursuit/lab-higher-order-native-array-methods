38d318a7483cbd2a450bedab5663b086d584a5dc
Phillip Andrew Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-02-19 16:05:10 -0500

    Passed

--- src/06-sort.js
+++ src/06-sort.js
@@ -13,7 +13,11 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  let sortedSongs = songs.sort((a,b) => (a.runtimeInSeconds > b.runtimeInSeconds) ? 1 : (a.runtimeInSeconds < b.runtimeInSeconds) ? -1 : 0 )
+
+  return sortedSongs
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,7 +27,23 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  return songs.sort((a,b) => {
+    return (b.artist.toUpperCase().localeCompare(a.artist.toUpperCase()));
+  })
+  
+  // return songs.sort((a,b) => {
+  //   if(b.artist.toUpperCase() < a.artist.toUpperCase()){
+  //     return -1
+  //   }
+  //   else if(b.artist.toUpperCase() > a.artist.toUpperCase()){
+  //     return 1
+  //   }
+  //   else{
+  //     return 0
+  //   }
+  // })
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +53,23 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  return songs.sort((a,b) => {
+    return (a.title.toUpperCase().localeCompare(b.title.toUpperCase()));
+  })
+  
+  // return songs.sort((a,b) => {
+  //   if(b.artist.toUpperCase() > a.artist.toUpperCase()){
+  //     return 1
+  //   }
+  //   else if(b.artist.toUpperCase() < a.artist.toUpperCase()){
+  //     return -1
+  //   }
+  //   else{
+  //     return 0
+  //   }
+  // })
+}
 
 module.exports = {
   sortByRuntimeAscending,
